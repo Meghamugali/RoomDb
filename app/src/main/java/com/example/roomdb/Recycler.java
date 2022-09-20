0a72894cd349d0b2d6aca69e2260ab0f089846f4
Megha <meghabmugali@gmail.com>
2022-09-20 15:27:37 +0530

    recycler roomdb working

--- app/src/main/java/com/example/roomdb/Recycler.java
+++ app/src/main/java/com/example/roomdb/Recycler.java
@@ -24,7 +24,7 @@ public class Recycler extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recycler);
 
         floatingActionButton = findViewById(R.id.addFAB);
 
